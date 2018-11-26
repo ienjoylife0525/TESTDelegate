bc214c144ad605f94cee74782410af913a4d3674
Patato <daniel.chang@git4u.com>
2018-11-26 17:04:58 +0800

    Finish rootPage & create First page Btn

--- TestDelegate/AppDelegate.swift
+++ TestDelegate/AppDelegate.swift
@@ -12,10 +12,17 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var rootpage: FirstPageViewController?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        rootpage = FirstPageViewController()
+        let nav: UINavigationController = UINavigationController(rootViewController: rootpage)
+        window?.rootViewController = nav
+        window!.makeKeyAndVisible()
+        
         return true
     }
 
--- TestDelegate/FirstPageViewController.swift
+++ TestDelegate/FirstPageViewController.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2018 Patato. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FirstPageViewController: UIViewController  {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "FirstPage"
+        self.view.backgroundColor = UIColor.white
+        
+        //換頁Button繪製
+        let switchBtn: UIButton = UIButton()
+        switchBtn.frame = CGRect(x: self.view.frame.size.width / 2 - 100, y: self.view.frame.size.height / 2 - 25, width: 200, height: 50)
+        switchBtn.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        switchBtn.setTitle("換頁", for: .normal)
+        switchBtn.setTitleColor(UIColor.white, for: .normal)
+        switchBtn.layer.cornerRadius = 10
+        switchBtn.backgroundColor = UIColor.darkGray
+        switchBtn.addTarget(self, action: #selector(FirstPageViewController.onClickToSecondPage(_:)), for: UIControl.Event.touchUpInside)
+        self.view.addSubview(switchBtn)
+        
+    }
+    
+    //切換頁
+    @objc func onClickToSecondPage(_ sender: UIButton) {
+    }
+    
+    
+    
+}
